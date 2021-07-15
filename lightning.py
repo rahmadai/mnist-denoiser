376b9d91b82e435d987a325c3880896d7b2db967
Rahmad Kurniawan <rahmadkurniawan.ai@gmail.com>
2021-07-15 13:53:16 +0700

    add MNIST Dataloader

--- lightning.py
+++ lightning.py
@@ -1,9 +1,13 @@
 import torch
 from torch.nn import functional as F
 from torch import nn
-from pytorch_lightning.core.lightning import LightningModule
+import pytorch_lightning as pl
+from torch.utils.data import DataLoader, random_split
+from torchvision.datasets import MNIST
+import os
+from torchvision import datasets, transforms
 
-class LitMNIST(LightningModule):
+class LitMNIST(pl.LightningModule):
   def __init__(self):
     super().__init__()
 
@@ -32,3 +36,36 @@ class LitMNIST(LightningModule):
       loss = F.nll_loss(logits, y)
       return loss
     
+class MNISTDataModule(pl.LightningDataModule):
+  def __init__(self, batch_size=64):
+      super().__init__()
+      self.batch_size = batch_size
+
+  def prepare_data(self):
+      MNIST(os.getcwd(), train=True, download=True, transform=transforms.ToTensor())
+      MNIST(os.getcwd(), train=False, download=True, transform=transforms.ToTensor())
+
+  def setup(self, stage=None):
+      # transform
+      transform=transforms.Compose([transforms.ToTensor()])
+      mnist_train = MNIST(os.getcwd(), train=True, download=False, transform=transform)
+      mnist_test = MNIST(os.getcwd(), train=False, download=False, transform=transform)
+
+      # train/val split
+      mnist_train, mnist_val = random_split(mnist_train, [55000, 5000])
+
+      # assign to use in dataloaders
+      self.train_dataset = mnist_train
+      self.val_dataset = mnist_val
+      self.test_dataset = mnist_test
+
+  def train_dataloader(self):
+      return DataLoader(self.train_dataset, batch_size=self.batch_size)
+
+  def val_dataloader(self):
+      return DataLoader(self.val_dataset, batch_size=self.batch_size)
+
+  def test_dataloader(self):
+      return DataLoader(self.test_dataset, batch_size=self.batch_size)
+    
+dm = MNISTDataModule()
