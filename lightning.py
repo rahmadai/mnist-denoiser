48c2bded172e35b67b6b0ef823f20a3b0f38c0ec
Rahmad Kurniawan <rahmadkurniawan.ai@gmail.com>
2021-07-15 14:03:16 +0700

    fit the MNISTModel and MNISTDataModule into trainer

--- lightning.py
+++ lightning.py
@@ -7,6 +7,9 @@ from torchvision.datasets import MNIST
 import os
 from torchvision import datasets, transforms
 
+AVAIL_GPUS = min(1, torch.cuda.device_count())
+BATCH_SIZE = 256 if AVAIL_GPUS else 64
+
 class LitMNIST(pl.LightningModule):
   def __init__(self):
     super().__init__()
@@ -35,6 +38,7 @@ class LitMNIST(pl.LightningModule):
       logits = self(x)
       loss = F.nll_loss(logits, y)
       return loss
+    
     
 class MNISTDataModule(pl.LightningDataModule):
   def __init__(self, batch_size=64):
@@ -69,3 +73,10 @@ class MNISTDataModule(pl.LightningDataModule):
       return DataLoader(self.test_dataset, batch_size=self.batch_size)
     
 dm = MNISTDataModule()
+model = LitMNIST()
+trainer = pl.Trainer(
+    max_epochs=2,
+    gpus=AVAIL_GPUS,
+    progress_bar_refresh_rate=20,
+)
+trainer.fit(model, dm)
